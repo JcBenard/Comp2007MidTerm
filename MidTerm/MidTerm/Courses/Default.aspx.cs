4e1dd4f4b96c828fb9db53bdb68975d58fe8963b
JcBenard <niteburn@hotmail.com>
2015-07-02 11:26:49 -0400

    Now redirects to the default table page and added files that weren't being tracked

---
+++ MidTerm/MidTerm/Courses/Default.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.Entity;
+using MidTerm.Models;
+
+namespace MidTerm.Courses
+{
+    public partial class Default : System.Web.UI.Page
+    {
+		protected MidTerm.Models.DefaultConnection _db = new MidTerm.Models.DefaultConnection();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        // Model binding method to get List of Cours entries
+        // USAGE: <asp:ListView SelectMethod="GetData">
+        public IQueryable<MidTerm.Models.Cours> GetData()
+        {
+            return _db.Courses;
+        }
+    }
+}
+
---
+++ MidTerm/MidTerm/Default.aspx
@@ -0,0 +1,8 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Default.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="MidTerm.Index" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <meta http-equiv="refresh" content="0; url=/Courses">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <a href="/Courses">Corusesu</a>
+    ghjghkhjlhjklhklhjklhklhkl
+</asp:Content>
